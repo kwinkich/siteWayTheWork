a9fb6ccb20f3f1b81b65c69577d4896b310384a5
kwinkich <vankord21@gmail.com>
2024-08-13 02:12:45 +0300

    update userid and added adaptive

--- src/components/ReadFirst/ReadFirst.component.tsx
+++ src/components/ReadFirst/ReadFirst.component.tsx
@@ -1,11 +1,13 @@
 import './_read-first.scss';
 
 export default function ReadFirst({ ...props }) {
+	const userId = new Date().getMilliseconds();
+
 	return (
 		<section className='container first'>
 			<div className='first__text-block'>
 				<h1 className='h1 white-text'>
-					Привет, <span className='primary-text'>user848586927!</span>
+					Привет, <span className='primary-text'>user{userId.toString()}!</span>
 				</h1>
 				<p className='first__description'>
 					Это простой сайт. Сюда я буду публиковатьы посты про то, что и как я
